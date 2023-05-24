3a234534a79134ac782c6b9fb77343a31798538b
Alvaro <almaflomon@gmail.com>
2023-05-23 22:53:36 -0400

    Library-default-example-done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgxCSVParserError, NgxCsvParser } from 'ngx-csv-parser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'csv-parser';
+  csvRecords: any;
+  header: boolean = false;
+
+  constructor(private ngxCsvParser: NgxCsvParser) {
+  }
+
+  @ViewChild('fileImportInput') fileImportInput: any;
+
+  fileChangeListener($event: any): void {
+
+    const files = $event.srcElement.files;
+    this.header = (this.header as unknown as string) === 'true' || this.header === true;
+
+    this.ngxCsvParser.parse(files[0],
+          {
+          header: this.header,
+          delimiter: ',',
+          encoding: 'utf8'
+          })
+      .pipe().subscribe({
+        next: (result): void => {
+          console.log('Result', result);
+          this.csvRecords = result;
+        },
+        error: (error: NgxCSVParserError): void => {
+          console.log('Error', error);
+        }
+      });
+  }
 }
